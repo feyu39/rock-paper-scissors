ecc8a7d882a8721a76f6a1dc8188724a2c356768
felixyu <yu.felix39@gmail.com>
2022-06-18 23:06:17 -0600

    fixed algorithm for buttons

--- script.js
+++ script.js
@@ -18,51 +18,105 @@ function playRound(playerSelection, computerSelection)
 {
     if(playerSelection === "Rock")
     {
-        if(computerSelection === "Paper") return printLosingGameResult(playerSelection, computerSelection);
-        else if (computerSelection === "Rock") return printDrawGameResult(playerSelection, computerSelection);
-        else return printWinningGameResult(playerSelection, computerSelection);
+        if(computerSelection === "Paper") return "Lose";
+        else if (computerSelection === "Rock") return "Draw";
+        else return "Win";
     }
     else if(playerSelection === "Scissors")
     {
-        if(computerSelection === "Rock") return printLosingGameResult(playerSelection, computerSelection);
-        else if (computerSelection === "Scissors") return printDrawGameResult(playerSelection, computerSelection);
-        else return printWinningGameResult(playerSelection, computerSelection);
+        if(computerSelection === "Rock") return "Lose";
+        else if (computerSelection === "Scissors") return "Win";
+        else return "Draw";
     }
     else if(playerSelection === "Paper")
     {
-        if(computerSelection === "Scissors") return printLosingGameResult(playerSelection, computerSelection);
-        else if (computerSelection === "Paper") return printDrawGameResult(playerSelection, computerSelection);
-        else return printWinningGameResult(playerSelection, computerSelection);
+        if(computerSelection === "Scissors") return "Lose";
+        else if (computerSelection === "Paper") return "Win";
+        else return "Draw";
     }
 }
 
-function game()
+function buttonPressed(e)
 {
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; ++i)
+    const button_name = this.getAttribute("class");
+    const output_string = document.querySelector(".output");
+    const player_score = document.querySelector(".playerScore");
+    const computer_score = document.querySelector(".computerScore");
+    const computerPlayFinal = computerPlay();
+
+    if(button_name === "btn-rock")
     {
-       let playerInput = prompt("Please type in your selection for Rock, Paper, Scissors: ");
-       let capitalizePlayerInput = capitalize(playerInput);
-
-       while((capitalizePlayerInput != "Rock") && (capitalizePlayerInput != "Scissors") && (capitalizePlayerInput != "Paper"))
-       {
-           let playerInput = prompt("Please type in your selection for Rock, Paper, Scissors correctly: " + "\n");
-           capitalizePlayerInput = capitalize(playerInput);
-       } 
-
-       let computerInput = computerPlay();
-       let roundString = playRound(capitalizePlayerInput, computerInput);
-
-       if(roundString === "You Win! " + capitalizePlayerInput + " beats " + computerInput) playerScore+=1;
-       else if(roundString === "You Lose! " + computerInput + " beats " + capitalizePlayerInput) computerScore+=1;
+        let round_result = playRound("Rock", computerPlayFinal);
+        if(round_result === "Lose")
+        {
+            output_string.textContent = printLosingGameResult("Rock", computerPlayFinal);
+            let computer_score_num = parseInt(computer_score.textContent, 10);
+            computer_score_num+=1;
+            computer_score.textContent = computer_score_num.toString();
+        }
+        else if(round_result === "Win")
+        {
+            output_string.textContent = printWinningGameResult("Rock", computerPlayFinal);
+            let player_score_num = parseInt(player_score.textContent, 10);
+            player_score_num+=1;
+            player_score.textContent = player_score_num.toString();
+        }
+        else{
+            output_string.textContent = printDrawGameResult("Rock", computerPlayFinal);
+        }
     }
-    if(computerScore > playerScore) console.log("Computer wins");
-    else if(playerScore > computerScore) console.log("Player wins");
-    else console.log("Draw");
-}
+    
+    else if(button_name === "btn-paper")
+    {
+        let round_result = playRound("Paper", computerPlayFinal);
+        if(round_result === "Lose")
+        {
+            output_string.textContent = printLosingGameResult("Paper", computerPlayFinal);
+            let computer_score_num = parseInt(computer_score.textContent, 10);
+            computer_score_num+=1;
+            computer_score.textContent = computer_score_num.toString();
+        }
+        else if(round_result === "Win")
+        {
+            output_string.textContent = printWinningGameResult("Paper", computerPlayFinal);
+            let player_score_num = parseInt(player_score.textContent, 10);
+            player_score_num+=1;
+            player_score.textContent = player_score_num.toString();
+        }
+        else{
+            output_string.textContent = printDrawGameResult("Paper", computerPlayFinal);
+        }
+    }
+    else if(button_name === "btn-scissors")
+    {
+        let round_result = playRound("Scissors", computerPlayFinal);
+        if(round_result === "Lose")
+        {
+            output_string.textContent = printLosingGameResult("Scissors", computerPlayFinal);
+            let computer_score_num = parseInt(computer_score.textContent, 10);
+            computer_score_num+=1;
+            computer_score.textContent = computer_score_num.toString();
+        }
+        else if(round_result === "Win")
+        {
+            output_string.textContent = printWinningGameResult("Scissors", computerPlayFinal);
+            let player_score_num = parseInt(player_score.textContent, 10);
+            player_score_num+=1;
+            player_score.textContent = player_score_num.toString();        }
+        else{
+            output_string.textContent = printDrawGameResult("Scissors", computerPlayFinal);
+        }    
+    }
 
-game();
+    if(Number(player_score.textContent) > 4) 
+    {
+        output_string.textContent = "Game Over, Player Wins!";
+    }
+    else if(Number(computer_score.textContent) > 4)
+    {
+        output_string.textContent = "Game Over, Computer Wins!"
+    }
+}
 
 function printWinningGameResult(playerSelection, computerSelection)
 {
@@ -76,6 +130,10 @@ function printDrawGameResult(playerSelection, computerSelection)
 {
     return "You Draw! " + computerSelection + " ties " + playerSelection;
 }
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", buttonPressed));
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
